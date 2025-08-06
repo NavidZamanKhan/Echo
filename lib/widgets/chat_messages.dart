d5d708ef2726426bb1e86b4c23898db46facc885
NavidZamanKhan <navid166418@gmail.com>
2025-08-06 08:44:53 +0600

    added push notification service

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:echo_chat_application/widgets/message_bubble.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
